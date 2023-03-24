b8fb9e9a74917475c57d7438334ee7bd39e4ae14
chris-marktopher <cmarktopher@gmail.com>
2023-03-25 09:07:13 +1100

    Updates

--- app/src/main/java/com/application/quizapp/MainActivity.java
+++ app/src/main/java/com/application/quizapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.application.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startQuizButtonElement;
+    private TextView nameInputTextElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get required references
+        startQuizButtonElement = findViewById(R.id.startButton);
+        nameInputTextElement = findViewById(R.id.nameInput);
+
+        // Bind the button on click event
+        startQuizButtonElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                intent.putExtra(getString(R.string.nameIntentData), nameInputTextElement.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/application/quizapp/QuizActivity.java
+++ app/src/main/java/com/application/quizapp/QuizActivity.java
@@ -2,13 +2,26 @@ package com.application.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView welcomeTextElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // Get required references
+        welcomeTextElement = findViewById(R.id.welcomeText);
+
+        // Get the intent so that we can use data sent from previous activity
+        Intent intent = getIntent();
+
+        // Set the welcome message
+        welcomeTextElement.setText("Welcome " + intent.getStringExtra(getString(R.string.nameIntentData)) + "!");
     }
 }
